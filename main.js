41a32ddc53c68ccb6b1262abe94f82292d0178ca
Sarkasaa <zockipod@gmail.com>
2018-12-17 02:03:09 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const taglines = [
+    "a cool guy",
+    "a gamer",
+    "an electrician",
+    "a lazy guy",
+    "also known as Nico"
+]
+
+let tagline = $("#tagline");
+tagline.html("Sarkasaa is " + taglines[Math.floor(Math.random() * taglines.length)] + " and this is his website!<br>Look around and see what you can find about me.");
+
+var btn = document.getElementById("dbtn");
+var clipboard = new ClipboardJS(dbtn);
