4f01bd338bd34e6a414d1a7734b216b86870b82d
Sarkasaa <zockipod@gmail.com>
2019-02-21 14:41:38 +0100

    moved errormsg and greeting to main.js

--- errormsg.js
+++
@@ -1,9 +0,0 @@
-const errormsgs = [
-    "Where are we?",
-    "How did you end up here?",
-    "Oh, that's not supposed to happen.",
-    "Huston, we have a problem."
-]
-
-let errormsg = $("#errormsg");
-errormsg.html(errormsgs[Math.floor(Math.random() * errormsgs.length)]);
--- main.js
+++ main.js
@@ -7,8 +7,55 @@ const taglines = [
     "always tired"
 ]
 
+const errormsgs = [
+    "Where are we?",
+    "How did you end up here?",
+    "Oh, that's not supposed to happen.",
+    "Huston, we have a problem.",
+    "Exsqueeze me?"
+]
+
+const n = new Date().getDay();
+
+// assign id for index.html doc
 let tagline = $("#tagline");
+let errormsg = $("#errormsg");
+let greeting = $("#greeting");
+
+// discord clipboard copy
+let btn = document.getElementById("dbtn");
+let clipboard = new ClipboardJS(dbtn);
+
+// is Wednesday?
+let greetings;
+let day = {
+    isWednesday: function () {
+        return n == 3;
+    }
+}
+
+
+// Taglines rand script
 tagline.html("Sarkasaa is " + taglines[Math.floor(Math.random() * taglines.length)] + " and this is his website!<br>Look around and see what you can find about me.");
 
-var btn = document.getElementById("dbtn");
-var clipboard = new ClipboardJS(dbtn);
+// Errormsg rand script
+errormsg.html(errormsgs[Math.floor(Math.random() * errormsgs.length)]);
+
+// Greetings table
+if (day.isWednesday()) {
+    greetings = ["It's Wednesday my dudes!!!"]
+} else {
+    greetings = [
+        "Henlo :D",
+        "'Sup?",
+        //"It's Wednesday my dudes",
+        "I have a boyfriend",
+        "The world is quiet here",
+        "What's the price of a mile?",
+        "I'm a tree",
+        "Your ass"
+    ]
+}
+
+// Greetings rand script
+greeting.html(greetings[Math.floor(Math.random() * greetings.length)]);
